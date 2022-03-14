99ecf95c1db396465eb9da39c386d44d7919aea3
Perfoys <prorok43battle@gmail.com>
2022-03-14 10:41:22 +0100

    Mock service worker setup

---
+++ src/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (request, response, context) => {
+    return response(
+      context.json(
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/vanilla.png" }
+      )
+    );
+  }),
+];
